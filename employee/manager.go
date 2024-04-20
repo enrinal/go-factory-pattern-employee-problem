de616afa117828067266796a54b3004f9cde70da
ShilmiR <shilmirohmahdiah09@gmail.com>
2024-04-20 04:27:49 +0100

    Update manager.go

--- employee/manager.go
+++ employee/manager.go
@@ -5,6 +5,7 @@ package employee
 // This means that Manager has all the fields and methods of Employee
 type Manager struct {
 	// TODO: Add the Employee struct
+	Employee
 }
 
 // NewManager creates a new manager
@@ -14,4 +15,16 @@ func NewManager() *Manager {
 	// TODO: Create a new manager
 	// Set the name to "Manager"
 	// Set the salary to 1000
+	manager := &Manager{
+		Employee: Employee{
+			Name:   "Manager",
+			Salary: 1000,
+			Bonus:  1000.0 * 0.2,
+		},
+	}
+	return manager
+}
+
+func (m *Manager) GetBonus() float64 {
+	return float64(m.Salary) * 0.2
 }
