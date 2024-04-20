b7ba39634aff688d2b8971f8d9e2c588c8677c17
Nayla Nur Fadhillah <nfnaylaa@gmail.com>
2024-04-20 09:40:31 +0700

    part 1

--- employee/manager.go
+++ employee/manager.go
@@ -4,7 +4,7 @@ package employee
 // It embeds Employee
 // This means that Manager has all the fields and methods of Employee
 type Manager struct {
-	// TODO: Add the Employee struct
+	Employee
 }
 
 // NewManager creates a new manager
@@ -14,4 +14,10 @@ func NewManager() *Manager {
 	// TODO: Create a new manager
 	// Set the name to "Manager"
 	// Set the salary to 1000
+	return &Manager{
+		Employee: Employee{
+			Name:   "Manager",
+			Salary: 1000,
+		},
+	}
 }
